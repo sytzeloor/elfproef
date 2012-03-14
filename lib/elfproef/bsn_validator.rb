2ed85224f962c5ac49a1d8a84a1bdd31af17a4a1
Ariejan de Vroom <ariejan@ariejan.net>
2012-03-14 16:29:07 +0100

    Updated docs for validate_with_advanced_eleven_test

--- lib/elfproef/bsn_validator.rb
+++ lib/elfproef/bsn_validator.rb
@@ -22,6 +22,20 @@ class BsnValidator < ActiveModel::EachValidator
     validate_with_advanced_eleven_test(bsn)
   end
 
+  # Performs the advanced 11-test on a
+  # 8 or 9 digit account number
+  #
+  #  For 9 digits (prefix with a 0 for 8 digits):
+  #
+  #   123456782
+  #   ABCDEFGHI
+  #
+  #   sum = 9*A + 8*B + 7*C + 6*D + 5*E + 4*F + 3*G + 2*H + -1*I
+  #
+  # Note the -1*I!
+  #
+  # If sum % 11 is 0, the number is valid, otherwise
+  # a typo has been made or the number is outright invalid.
   def validate_with_advanced_eleven_test(number)
     # Make sure we have 9 digits
     number = "0#{number}" if number.size == 8
